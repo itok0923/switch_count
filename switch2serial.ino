2fe4d386cdf632d769a13121a421e24de09f3c9c
itok0923 <k.ito@showa-ele.jp>
2021-05-14 15:11:59 +0900

    新規

---
+++ switch2serial.ino
@@ -0,0 +1,37 @@
+#define SWITCH_PIN PA0
+#define HIGH_VALUE 1020 //通電時1024~1023
+#define LOW_VALUE 50 //非通電時0~1
+
+int switchstat[2];
+int count[2]={0,0}; //{on,off}回数
+
+void setup() {
+  Serial.begin(115200UL);
+  Serial.println("switch count");
+  pinMode(SWITCH_PIN,INPUT);
+  switchstat[1]=analogRead(SWITCH_PIN);
+}
+
+void loop() {
+  switch_change();
+}
+void switch_change(){
+  switchstat[0]=analogRead(SWITCH_PIN);
+  if((switchstat[0] < LOW_VALUE) && (switchstat[1] > HIGH_VALUE)){
+    count[0]++;
+    count_print();
+  }else if((switchstat[0] > HIGH_VALUE) && (switchstat[1] < LOW_VALUE)){
+    count[1]++;
+    count_print();
+  }else{
+  }
+  switchstat[1]=switchstat[0];
+  delay(50);
+}
+void count_print(){
+  Serial.print(" on count:");
+  Serial.println(count[0]);
+  Serial.print("off count:");
+  Serial.println(count[1]);
+  Serial.println("------");
+}
